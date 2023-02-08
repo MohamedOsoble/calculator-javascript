bef7bbfdff2fa2fcb7b3ad6df70cf37faee179c9
Mohamed Osoble <mohamedosoble@outlook.com>
2023-02-08 12:51:55 +0000

    test script functionality

--- javascript.js
+++ javascript.js
@@ -2,19 +2,27 @@
 // addition function
 function add(a, b){
     return a + b;
-}
+};
 
 // subtract function
 function subtract(a, b){
     return a - b;
-}
+};
 
 // division function
 function divide(a, b){
     return a/b;
-}
+};
 
 // multiplication function
 function multiply(a, b){
     return a*b;
-}
+};
+
+console.log(add(5, 4));
+
+console.log(subtract(20, 15));
+
+console.log(multiply(15, 4));
+
+console.log(divide(28, 7));
