7af27272b7a53f0429ec1672182f03ece3e1c082
Mohamed Osoble <mohamedosoble@outlook.com>
2023-02-08 12:45:54 +0000

    add basic operation script functionality to javascript.js

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+// addition function
+function add(a, b){
+    return a + b;
+}
+
+// subtract function
+function subtract(a, b){
+    return a - b;
+}
+
+// division function
+function divide(a, b){
+    return a/b;
+}
+
+// multiplication function
+function multiply(a, b){
+    return a*b;
+}
